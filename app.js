0a7f950c4438baaa544a61fcbb116ed1e18d125a
Alex Dee <adchengzhi7@gmail.com>
2025-06-26 21:26:22 +0800

    cors solve

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var cors =  require('cors');
+var corsMiddleware = require('./cors');
 
 
 var indexRouter = require('./routes/index');
@@ -16,8 +16,10 @@ require('dotenv').config();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(cors());
-app.options('*',cors());
+
+app.use(corsMiddleware);
+app.options('*', corsMiddleware);
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- cors/index.js
+++ cors/index.js
@@ -1,8 +1,12 @@
+// cors/index.js
 const cors = require('cors');
 
 const corsOptions = {
-    origin: 'https://mbastudent.nccu.edu.tw/',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  }
+  origin: 'https://mbastudent.nccu.edu.tw',       // ← 不能有多餘的 '/'
+  credentials: true,                              // ← 如果你要帶 cookie/session
+  methods: ['GET', 'POST', 'OPTIONS'],            // ← 明訂允許的 HTTP method
+  allowedHeaders: ['Content-Type', 'Authorization'], // ← 明訂允許的 header
+  optionsSuccessStatus: 200
+};
 
-module.exports = cors(corsOptions);  
+module.exports = cors(corsOptions);
